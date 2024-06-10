6b6a1f49ecb0cbb1f14609d508e03a19192d954b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-10 23:15:49 +0530

    first

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const fs = require('fs');
+const fs = require("fs");
+
+const mesg = "Hello, World!";
+
+fs.writeFile("output.txt", mesg, (err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("Success");
+});
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-
